c17115df187b11358aa9ef5dcf0948babb6b9145
키리토 <nno88551@naver.com>
2014-02-12 12:55:03 +0900

    [수정및추가]양동이들고 소 누르면 우유,버그수정,이름변경

--- plugins/EntityPlugin.php
+++ plugins/EntityPlugin.php
@@ -2,16 +2,16 @@
 
 /*
 __PocketMine Plugin__
-name=AutoSpawn
+name=EntityPlugin
 description=
 version=
 author=KoMC
-class=AutoSpawn
+class=EntityPlugin
 apiversion=11,12,13
 */
 
 
-class MilkRPG implements Plugin{
+class EntityPlugin implements Plugin{
 	private $api;
 	private $server;
 	public function __construct(ServerAPI $api, $server = false){
@@ -22,6 +22,7 @@ class MilkRPG implements Plugin{
 	public function __destruct(){}
 	public function init(){
 		$this->api->addHandler("entity.death", array($this, "han"));
+		$this->api->addHandler("player.interact", array($this, "han"));
 		$this->conf = new Config($this->api->plugin->configPath($this)."config.yml",CONFIG_YAML, array(
 			"스폰시간" => 15,//20이 1초
 			"몬스터최대수" => 25,
@@ -31,28 +32,22 @@ class MilkRPG implements Plugin{
 	
 	public function han($data, $event){
 		switch($event){
-			case "Monster.Spawn":
-				foreach($this->api->level->getAll() as $level){
-					if($this->api->time->getPhase($level) == "night"){//밤일시에 몬스터 스폰
-						if($this->spawn < $this->conf->get("몬스터최대수")){
-							$mobrand = mt_rand(32,36);
-							$data = array(
-								"x" => mt_rand(0,25580)/100,
-								"y" => mt_rand(50,127),
-								"z" => mt_rand(0,25580)/100,
-							);
-							$block = $level->getBlock(new Vector3($data["x"],$data["y"],$data["z"]));
-							$blockd = $level->getBlock(new Vector3($data["x"],$data["y"]-1,$data["z"]));
-							if(($block->getID() == 0 or !$block->isFullBlock)
-							and $blockd->getID() != 0 and $blockd->isFullBlock){
-								$this->spawn++;
-								$e = $this->api->entity->add($level, ENTITY_MOB, $mobrand, $data);
-								$this->api->entity->spawnToAll($e);
-							}
-						}
+			case "player.interact":
+				$entity = $data["targetentity"];
+				$player = $data["entity"]->player;
+				if($data["action"] == 1){
+					$slot = $player->getSlot($player->slot);
+					if($slot->getID() == BUCKET and $entity->type == COW){
+						$player->setSlot($player->slot,new Item(BUCKET,1,1),false);
 					}
+					return false;
+				}
+				break;
+			case "Monster.Spawn":
+				$level = $this->api->level->getDefault();
+				if($this->api->time->getPhase($level) == "night"){//밤일시에 몬스터 스폰
 					if($this->spawn < $this->conf->get("몬스터최대수")){
-						$mobrand = mt_rand(10,13);
+						$mobrand = mt_rand(32,36);
 						$data = array(
 							"x" => mt_rand(0,25580)/100,
 							"y" => mt_rand(50,127),
@@ -66,6 +61,22 @@ class MilkRPG implements Plugin{
 							$e = $this->api->entity->add($level, ENTITY_MOB, $mobrand, $data);
 							$this->api->entity->spawnToAll($e);
 						}
+					}
+				}
+				if($this->spawn < $this->conf->get("몬스터최대수")){
+					$mobrand = mt_rand(10,13);
+					$data = array(
+						"x" => mt_rand(0,25580)/100,
+						"y" => mt_rand(50,127),
+						"z" => mt_rand(0,25580)/100,
+					);
+					$block = $level->getBlock(new Vector3($data["x"],$data["y"],$data["z"]));
+					$blockd = $level->getBlock(new Vector3($data["x"],$data["y"]-1,$data["z"]));
+					if(($block->getID() == 0 or !$block->isFullBlock)
+					and $blockd->getID() != 0 and $blockd->isFullBlock){
+						$this->spawn++;
+						$e = $this->api->entity->add($level, ENTITY_MOB, $mobrand, $data);
+						$this->api->entity->spawnToAll($e);
 					}
 				}
 				break;
